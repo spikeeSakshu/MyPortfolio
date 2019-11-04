44787bd2106235c747044f0b143e51df5636bed8
spikeeSakshu <spikeesaksu@gmail.com>
2019-11-05 03:49:42 +0530

    Updated Preloader

--- js/app.js
+++ js/app.js
@@ -12,10 +12,60 @@
          * Preloader
          ***********************************************/
 
-        jQuery(window).load(function () {
-            jQuery("#status").fadeOut();
-            jQuery("#preloader").delay(1000).fadeOut("slow");
-        })
+        // jQuery(window).load(function () {
+        //     jQuery("#status").fadeOut();
+        //     jQuery("#preloader").delay(1000).fadeOut("slow");
+        // })
+
+        /*** Preloader ***/
+	
+	function grax_preloader(){
+		
+        var mainPreloader	 = $(".grax_loader-wrapper .loader");
+        var WinWidth 		 = $(window).width();
+        var WinHeight		 = $(window).height();
+        var zero = 0;
+    
+        mainPreloader.css({
+            top: WinHeight / 2 - 2.5,
+            left: WinWidth / 2 - 200
+        });
+    
+        do {
+            mainPreloader.animate({
+                width: zero
+            }, 10);
+            zero += 3;
+        } while (zero <= 400);
+        if (zero === 402) {
+            mainPreloader.animate({
+                left: 0,
+                width: '100%'
+            });
+            mainPreloader.animate({
+                top: '0',
+                height: '100vh'
+            });
+        }
+            
+        setTimeout(function() {
+            $(".grax_loader-wrapper").fadeOut('fast');
+            (mainPreloader).fadeOut('fast');
+        }, 4500);
+    }
+        jQuery(window).on('scroll',function(){
+            grax_modal_scroll();
+            renex_hero_effect();
+        });
+    
+        jQuery(window).on('resize',function(){
+            grax_hero_height();
+        });
+    
+        jQuery(window).load('body', function(){
+            setTimeout(function(){grax_preloader();},1000);
+        });
+        
 
         /***********************************************
          * Universal Parallax
